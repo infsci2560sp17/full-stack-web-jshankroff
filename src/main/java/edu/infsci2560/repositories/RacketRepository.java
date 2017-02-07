52eabd35ddd756211b4199367560f01bbe103586
jshankroff <jts89@pitt.edu>
2017-02-07 20:01:18 +0000

    Added RacketRepository.java

---
+++ src/main/java/edu/infsci2560/repositories/RacketRepository.java
@@ -0,0 +1,17 @@
+
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.infsci2560.repositories;
+
+import edu.infsci2560.models.TennisRackets;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ *
+ * @author kolobj
+ */
+public interface RacketRepository extends PagingAndSortingRepository<TennisRackets, Long> {}
+
