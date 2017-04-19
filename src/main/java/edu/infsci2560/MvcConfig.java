1be65175f16ef8ddb3677edf2b56b0e6a6d24a92
jshankroff <jts89@pitt.edu>
2017-04-19 19:27:08 +0000

    edits to edit html pages, and mvcConfig

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -13,6 +13,12 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/balls").setViewName("balls");
+        registry.addViewController("/overgrips").setViewName("overgrips");
+        registry.addViewController("/rackets").setViewName("rackets");
+        registry.addViewController("/replacementgrips").setViewName("replacementgrips");
+        registry.addViewController("/shoes").setViewName("shoes");
+        registry.addViewController("/strings").setViewName("strings");
     }
 
 }
