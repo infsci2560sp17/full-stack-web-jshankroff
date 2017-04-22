911fcc1e7bd8043dcf2b6b6efe71a3a748b98d13
jshankroff <jts89@pitt.edu>
2017-04-22 00:04:56 +0000

    css/html updates

--- src/main/java/edu/infsci2560/FullStackWebApplication.java
+++ src/main/java/edu/infsci2560/FullStackWebApplication.java
@@ -1,15 +1,34 @@
 package edu.infsci2560;
 
 import edu.infsci2560.models.Customer;
+import edu.infsci2560.models.TennisBalls;
+import edu.infsci2560.models.TennisBalls.BallType;
+import edu.infsci2560.models.TennisRackets;
+import edu.infsci2560.models.TennisRackets.RacketType;
+import edu.infsci2560.models.TennisOvergrips;
+import edu.infsci2560.models.TennisOvergrips.OvergripType;
+import edu.infsci2560.models.TennisReplacementgrips;
+import edu.infsci2560.models.TennisReplacementgrips.ReplacementgripType;
+import edu.infsci2560.models.TennisShoes;
+import edu.infsci2560.models.TennisShoes.ShoeType;
+import edu.infsci2560.models.TennisStrings;
+import edu.infsci2560.models.TennisStrings.StringType;
+
 import edu.infsci2560.repositories.CustomerRepository;
+import edu.infsci2560.repositories.BallRepository;
+import edu.infsci2560.repositories.RacketRepository;
+import edu.infsci2560.repositories.OvergripRepository;
+import edu.infsci2560.repositories.ShoeRepository;
+import edu.infsci2560.repositories.StringRepository;
+import edu.infsci2560.repositories.ReplacementgripRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class FullStackWebApplication {
@@ -17,8 +36,24 @@ public class FullStackWebApplication {
     private static final Logger log = LoggerFactory.getLogger(FullStackWebApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(FullStackWebApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(FullStackWebApplication.class, args);
+    
+        BallRepository ballRepo = ctx.getBean(BallRepository.class);
+        ballRepo.save(new TennisBalls(1L, "Normal", BallType.Slazenger));
+        
+        OvergripRepository overgripRepo = ctx.getBean(OvergripRepository.class);
+        overgripRepo.save(new TennisOvergrips(1L, "Pro", OvergripType.Wilson));
+        
+        RacketRepository racketRepo = ctx.getBean(RacketRepository.class);
+        racketRepo.save(new TennisRackets(1L, "Blade 16x19", RacketType.Wilson));
+        racketRepo.save(new TennisRackets(1L, "Pure drive 16x19", RacketType.Babolat));
+        
+        ReplacementgripRepository replacementgripRepo = ctx.getBean(ReplacementgripRepository.class);
+        replacementgripRepo.save(new TennisReplacementgrips(1L, "Grip", ReplacementgripType.Wilson));
+        
+        
     }
+}
 
 //    @Bean
 //    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
@@ -34,7 +69,7 @@ public class FullStackWebApplication {
 //
 //        };
 //    }
-    @Bean
+ /*   @Bean
     public CommandLineRunner databaseDemo(CustomerRepository repository) {
         return (args) -> {
             // save a couple of customers
@@ -67,4 +102,4 @@ public class FullStackWebApplication {
             }            
         };
     }
-}
+}*/
